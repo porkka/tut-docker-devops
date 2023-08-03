08cbefc322b02c85b59c4c5322d4cd4dd1d53609
Teppo <47066328+porkka@users.noreply.github.com>
2023-08-03 10:25:18 +0300

    Added docker compose files to docker ignore file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h2>Hello world!</h2>");
+  res.send("<h2>Hello world</h2>");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
