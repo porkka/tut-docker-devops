def2a8121e925ddb265508a6735a027548b29408
Teppo <47066328+porkka@users.noreply.github.com>
2023-08-03 14:07:36 +0300

    Connected to mongo db

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const e = require("express");
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose
+  .connect("mongodb://pelaaja2:munpassu@mongo:27017/?authSource=admin")
+  .then(() => console.log(">> Database connected"))
+  .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   res.send("<h2>Hello world</h2>");
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`>> Listening on port ${port}`));
