40a36ed545404fc796d8d714605ad3cd8bd684ff
Teppo <47066328+porkka@users.noreply.github.com>
2023-08-03 15:05:13 +0300

    Added boot order and connection retry function

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Docker tutorial
 
 DevOps with Node.js & Express  
-https://youtu.be/9zUHg7xjIqQ?t=8440
+https://youtu.be/9zUHg7xjIqQ?t=9147
 
 `docker run -d -v "$(pwd)":/app:ro -v /app/node_modules -p 3000:4000 --env-file ./.env --name node-app node-app-image`
--- index.js
+++ index.js
@@ -11,10 +11,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-mongoose
-  .connect(mongoURL)
-  .then(() => console.log(">> Database connected"))
-  .catch((e) => console.log(e));
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL)
+    .then(() => console.log(">> Database connected"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
   res.send("<h2>Hello world</h2>");
